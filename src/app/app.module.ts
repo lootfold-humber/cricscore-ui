8d590ef3ac80958026f5c321972f74f7505b337c
Pallav Dubey <pallav.dubey@gmail.com>
2022-06-04 18:20:29 -0400

    add links to dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { TeamsComponent } from './teams/teams.component';
+import { MatchesComponent } from './matches/matches.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +17,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     LoginComponent,
     SignUpComponent,
     DashboardComponent,
+    TeamsComponent,
+    MatchesComponent,
   ],
   imports: [
     BrowserModule,
@@ -24,6 +28,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
       { path: '', component: LoginComponent },
       { path: 'signup', component: SignUpComponent },
       { path: 'dashboard', component: DashboardComponent },
+      { path: 'teams', component: TeamsComponent },
+      { path: 'matches', component: MatchesComponent },
     ]),
   ],
   providers: [],
