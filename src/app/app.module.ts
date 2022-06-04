ff044fa939009f40d7e8ae430bf5f35c1f24afa7
Pallav Dubey <pallav.dubey@gmail.com>
2022-06-04 17:25:03 -0400

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
-  declarations: [AppComponent, SignUpComponent],
-  imports: [BrowserModule, ReactiveFormsModule, HttpClientModule],
+  declarations: [AppComponent, LoginComponent, SignUpComponent],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: '', component: LoginComponent },
+      { path: 'signup', component: SignUpComponent },
+    ]),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
